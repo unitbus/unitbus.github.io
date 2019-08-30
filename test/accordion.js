e1c7125099a4a05cfa4719afab2cd26c6266394c
unitbus <co.unitbus@gmail.com>
2019-08-31 05:32:48 +0900

    update menu.

--- test/accordion.js
+++ test/accordion.js
@@ -1,5 +1,19 @@
-$(function() {
-    $(".accordion").click(function() {
-        $(this).toggleClass("close").next().slideToggle();
-    });
+new Vue({
+	el:"#app",
+  data:{
+  	listlabel:["Mens","Womens","Kids"],
+    sublistlabel:["Cloth","Pants","Shoes","Goods"],
+    active:""
+  },
+  methods:{
+  	dropdown(list){
+    console.log(this.active);
+    	this.active = this.active===list ? "":list;
+    }
+  }
+});
+
+new Vue({
+  el: '#app',
+  mixins: [accordion], // mixinで読み込む
 });
